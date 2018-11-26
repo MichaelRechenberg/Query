6b385adbb3ff8f6d0d0b71075ccb9c9d88fb1351
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2018-11-26 16:52:50 -0600

    added class and lecture list activities

---
+++ app/src/main/java/com/example/conwayying/query/ClassListActivity.java
@@ -0,0 +1,13 @@
+package com.example.conwayying.query;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClassListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_list);
+    }
+}
