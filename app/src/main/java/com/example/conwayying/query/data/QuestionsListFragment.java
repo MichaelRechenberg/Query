d717843333c9e91516cf85e81f69a3d7c7d182db
MichaelRechenberg <michaelrechenberg@oawl.net>
2018-12-06 13:25:15 -0600

    Ability to sort questions and timestamps via a spinner (spinner purposely not included in activity_main.xml to prevent merge conflicts...but TODO will later have to be hooked up to some spinner with the sort_list_menu_choices string-array included in this commit's strings.xml)
    QuestionListFragment and TimestampsListFragment maintain additional state: the criteria by which they are currently sorting their elements. Hacky but it works
    
    Refactored how refreshing works in QuestionsListFragment and TimestampsListFragment
    
    All buttons in the ButtonFragment will switch the list fragment and set the sort spinner to "Date Created" when their respective entities are inserted.
    
    Fixed bug where when you opened the MainActivity for the first time, the timestamps list fragment was created but the "Questions" option in the bottom navigation bar was selected

--- app/src/main/java/com/example/conwayying/query/data/QuestionsListFragment.java
+++ app/src/main/java/com/example/conwayying/query/data/QuestionsListFragment.java
@@ -10,14 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import com.example.conwayying.query.R;
 import com.example.conwayying.query.data.entity.Note;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,11 +30,20 @@ import java.util.List;
 // https://gist.github.com/ar-android/002d77cbd935f970ef5c
 public class QuestionsListFragment extends android.support.v4.app.ListFragment {
 
+    public final static int SORT_BY_IS_RESOLVED = 1;
+    public final static int SORT_BY_TIME_CREATED = 2;
+
+    private final static String[] from = {"Note Text", "Private Status", "Is Resolved Status"};
+    private final static int[] to = {R.id.firstLine, R.id.secondLine, R.id.thirdLine};
+
     private String dataArray[];
     private ArrayList<HashMap<String, String>> data;
     private QueryAppRepository qar;
     private QuestionClicked mCallback;
     private SimpleAdapter adapter;
+
+    // What SORT_BY* criteria we are using for the list of questions
+    private int mSortCriteria;
 
     public QuestionsListFragment() {
         // required empty constructor
@@ -92,43 +100,33 @@ public class QuestionsListFragment extends android.support.v4.app.ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        int lectureId = this.getArguments().getInt("LectureId", -1);
-        List<Note> notesList = qar.getAllNotesForLecture(lectureId);
 
-        // We populate the data array list
-        // Each hash map in the array list represents a single row's information
-        HashMap<String, String> rowInformation;
-        // Clear data
-        data = new ArrayList<>();
-
-        for (Note note : notesList) {
-            rowInformation = new HashMap<String, String>();
-            rowInformation.put("Note Text", note.getNoteText());
-            rowInformation.put("Private Status", note.getIsPrivate() ? "Private" : "Public");
-            rowInformation.put("Is Resolved Status", note.getIsResolved() ? "Resolved" : "Unresolved");
-            rowInformation.put("Slide Number", Integer.toString(note.getSlideNumber()));
-            rowInformation.put("id", Integer.toString(note.getNoteId()));
-            data.add(rowInformation);
-        }
-
-        String[] from = {"Note Text", "Private Status", "Is Resolved Status"};
-
-        int[] to = {R.id.firstLine, R.id.secondLine, R.id.thirdLine};
+        // default to sorting questions by time created
+        mSortCriteria = SORT_BY_TIME_CREATED;
 
         adapter = new SimpleAdapter(getActivity(), data, R.layout.question_fragment_list_item, from, to);
         setListAdapter(adapter);
+
+        refreshQuestionList(mSortCriteria);
     }
 
-    public void refreshQuestionsFragment() {
-        // Refreshes the fragment stuff
-        Log.d("Refreshing Notes", "Let's go");
-        String[] from = {"Note Text", "Private Status", "Is Resolved Status"};
+    /**
+     * Refresh the list of questions, sorting the list based on the sortBy parameter
+     * @param sortBy QuestionListFragment.SORT_BY_DATE_CREATED or QuestionListFragment.SORT_BY_RESOLVED_STATE
+     */
+    public void refreshQuestionList(int sortBy) {
 
-        int[] to = {R.id.firstLine, R.id.secondLine, R.id.thirdLine};
+        mSortCriteria = sortBy;
+
+        // Refreshes the fragment stuff and sorts
+        Log.d("Refreshing Notes", "Let's go, sorting using criteria " + mSortCriteria);
+
         int lectureId = this.getArguments().getInt("LectureId", -1);
-        List<Note> notesList = qar.getAllNotesForLecture(lectureId);
-        HashMap<String, String> rowInformation;
 
+        ArrayList<Note> notesList = (ArrayList<Note>) qar.getAllNotesForLecture(lectureId);
+        sortNotes(sortBy, notesList);
+
+        HashMap<String, String> rowInformation;
         data.clear();
 
         for (Note note : notesList) {
@@ -141,6 +139,59 @@ public class QuestionsListFragment extends android.support.v4.app.ListFragment {
             data.add(rowInformation);
         }
         adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Refresh the questions list, using the current sorting criteria of the list
+     */
+    public void refreshQuestionList(){
+       refreshQuestionList(this.mSortCriteria);
+    }
+
+    /**
+     * Sorts the input array of notes based on sortBy attribute
+     *
+     * SORT_BY_IS_RESOLVED: All unresolved notes show up first, breaking ties by dateCreated
+     * SORT_BY_START_DATE: Sort by dateCreated
+     * @param sortBy sorting condition
+     * @param notes Array of notes to sort...modified by this function
+     */
+    private void sortNotes(int sortBy, ArrayList<Note> notes){
+        switch (sortBy){
+            case SORT_BY_IS_RESOLVED:
+                notes.sort(new Comparator<Note>() {
+                    @Override
+                    public int compare(Note note1, Note note2) {
+                        boolean note1Resolved = note1.getIsResolved();
+                        boolean note2Resolved = note2.getIsResolved();
+
+                        // If resolved status is the same, use date created to tie break
+                        if (note1Resolved == note2Resolved){
+                            return note1.getDateCreated().compareTo(note2.getDateCreated());
+                        }
+                        // If note 1 is resolved and note2 is not, have note2 show up first in the ordering
+                        else if (note1Resolved && !note2Resolved){
+                            return 1;
+                        }
+                        else{
+                            return -1;
+                        }
+                    }
+                });
+
+                break;
+            case SORT_BY_TIME_CREATED:
+                notes.sort(new Comparator<Note>() {
+                    @Override
+                    public int compare(Note note1, Note note2) {
+                        return note1.getDateCreated().compareTo(note2.getDateCreated());
+                    }
+                });
+                break;
+            default:
+                Log.e("Foo", "Invalid sorting criteria given to sorting questions list");
+                break;
+        }
     }
 
     @Override
@@ -155,28 +206,8 @@ public class QuestionsListFragment extends android.support.v4.app.ListFragment {
             qar.updateNoteIsResolved(Integer.parseInt(data.get(position).get("id")), true);
         }
 
-        // Refreshes the fragment stuff
-        Log.d("Refreshing Notes", "Let's go");
-        String[] from = {"Note Text", "Private Status", "Is Resolved Status"};
-
-        int[] to = {R.id.firstLine, R.id.secondLine, R.id.thirdLine};
-        int lectureId = this.getArguments().getInt("LectureId", -1);
-        List<Note> notesList = qar.getAllNotesForLecture(lectureId);
-        HashMap<String, String> rowInformation;
-
-        data.clear();
-
-        for (Note note : notesList) {
-            rowInformation = new HashMap<String, String>();
-            rowInformation.put("Note Text", note.getNoteText());
-            rowInformation.put("Private Status", note.getIsPrivate() ? "Private" : "Public");
-            rowInformation.put("Is Resolved Status", note.getIsResolved() ? "Resolved" : "Unresolved");
-            rowInformation.put("Slide Number", Integer.toString(note.getSlideNumber()));
-            rowInformation.put("id", Integer.toString(note.getNoteId()));
-            data.add(rowInformation);
-        }
-        adapter.notifyDataSetChanged();
-
+        // Comment out if we want to refresh and re-sort the question list
+        refreshQuestionList();
     }
 
     public void clickedOn(int slide) {
--- app/src/main/java/com/example/conwayying/query/data/TimestampsListFragment.java
+++ app/src/main/java/com/example/conwayying/query/data/TimestampsListFragment.java
@@ -1,6 +1,5 @@
 package com.example.conwayying.query.data;
 
-import android.app.Activity;
 import android.app.ListFragment;
 import android.content.Context;
 import android.net.Uri;
@@ -9,18 +8,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import com.example.conwayying.query.R;
 import com.example.conwayying.query.data.entity.ConfusionMark;
-import com.example.conwayying.query.data.entity.Note;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -36,13 +32,25 @@ import java.util.List;
 // https://gist.github.com/ar-android/002d77cbd935f970ef5c
 public class TimestampsListFragment extends android.support.v4.app.ListFragment {
 
-    private String dataArray[];
+
+    // Sorting criteria for ConfusionMarks/Timestamps
+    // Sort by unresolved, then resolved ConfusionMarks...breaking ties with dateCreated
+    public final static int SORT_BY_IS_RESOLVED = 1;
+    // Sort by startDate
+    public final static int SORT_BY_START_DATE = 2;
+
+    private static final String[] from = {"Confusion Mark", "Is Resolved Status"};
+    private static final int[] to = {R.id.confusionMarkText, R.id.thirdLine};
+
     private ArrayList<HashMap<String, String>> data;
     private QueryAppRepository qar;
     private OnFragmentInteractionListener mListener;
     private SimpleAdapter adapter;
 
     private TimestampClicked mCallback;
+
+    // Current sorting criteria of Timestamps
+    private int mSortCriteria;
 
     public TimestampsListFragment (){
         // required empty constructor
@@ -100,41 +108,13 @@ public class TimestampsListFragment extends android.support.v4.app.ListFragment 
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        int lectureId = this.getArguments().getInt("LectureId", -1);
-        List<ConfusionMark> confusionMarksList = qar.getAllConfusionMarksForLecture(lectureId);
-
-        // We populate the data array list
-        // Each hash map in the array list represents a single row's information
-        HashMap<String, String> rowInformation;
-        // Clear data
-        data = new ArrayList<>();
-
-        for (ConfusionMark confusionMark : confusionMarksList) {
-            rowInformation = new HashMap<String, String>();
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-            String concatenatedStartEnd;
-            if (confusionMark.getEndDate() == null) {
-                concatenatedStartEnd = sdf.format(confusionMark.getStartDate());
-                Log.d("lol", "falling");
-            }
-            else {
-                concatenatedStartEnd = sdf.format(confusionMark.getStartDate()) + " - " + sdf.format(confusionMark.getEndDate());
-            }
-            rowInformation.put("Confusion Mark", concatenatedStartEnd);
-            rowInformation.put("Slide Number", Integer.toString(confusionMark.getSlideNumber()));
-            rowInformation.put("Is Resolved Status", confusionMark.getIsResolved() ? "Resolved" : "Unresolved");
-            rowInformation.put("isResolved", String.valueOf(confusionMark.getIsResolved()));
-            rowInformation.put("id", Integer.toString(confusionMark.getConfusionId()));
-
-            data.add(rowInformation);
-        }
-
-        String[] from = {"Confusion Mark", "Is Resolved Status"};
-
-        int[] to = {R.id.confusionMarkText, R.id.thirdLine};
-
         adapter = new SimpleAdapter(getActivity(), data, R.layout.timestamps_fragment_list_item, from, to);
         setListAdapter(adapter);
+
+        // Default to time created
+        mSortCriteria = TimestampsListFragment.SORT_BY_START_DATE;
+        // Pull timestamps from DB and sort
+        refreshTimestampsList(mSortCriteria);
     }
 
     @Override
@@ -149,19 +129,31 @@ public class TimestampsListFragment extends android.support.v4.app.ListFragment 
 
         // Refreshes the fragment stuff
         Log.d("Refreshing Timestamps", "Let's go");
-        String[] from = {"Confusion Mark", "Is Resolved Status"};
-        int[] to = {R.id.confusionMarkText, R.id.thirdLine};
+
+        // Comment out if we want to refresh and re-sort the list onListItemClick
+        refreshTimestampsList(this.mSortCriteria);
+    }
+
+    /**
+     * Refresh the list of timestamps by pulling from the DB, specifying a sorting criteria
+     * @param sortBy SORT_BY_IS_RESOLVED or SORT_BY_START_DATE
+     */
+    public void refreshTimestampsList(int sortBy){
+
+        this.mSortCriteria = sortBy;
 
         int lectureId = this.getArguments().getInt("LectureId", -1);
-        List<ConfusionMark> confusionMarksList = qar.getAllConfusionMarksForLecture(lectureId);
+        ArrayList<ConfusionMark> confusionMarksList = (ArrayList<ConfusionMark>) qar.getAllConfusionMarksForLecture(lectureId);
+        sortConfusionMarks(mSortCriteria, confusionMarksList);
 
-        HashMap<String, String> rowInformation;
+        // We populate the data array list
 
-        // Need this to keep same reference
+        // Clear data
         data.clear();
 
         for (ConfusionMark confusionMark : confusionMarksList) {
-            rowInformation = new HashMap<String, String>();
+            // Each hash map in the array list represents a single row's information
+            HashMap<String, String> rowInformation = new HashMap<String, String>();
             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
             String concatenatedStartEnd;
             if (confusionMark.getEndDate() == null) {
@@ -179,8 +171,63 @@ public class TimestampsListFragment extends android.support.v4.app.ListFragment 
 
             data.add(rowInformation);
         }
+
         adapter.notifyDataSetChanged();
     }
+
+    /**
+     * Refresh the timestamps list, using the current sorting criteria
+     */
+    public void refreshTimestampsList(){
+       refreshTimestampsList(this.mSortCriteria);
+    }
+
+    /**
+     * Sorts the input array of notes based on sortBy attribute
+     *
+     * SORT_BY_IS_RESOLVED: All unresolved notes show up first, breaking ties by dateCreated
+     * SORT_BY_START_DATE: Sort by dateCreated
+     * @param sortBy sorting condition
+     * @param confusionMarks Array of notes to sort...modified by this function
+     */
+    private void sortConfusionMarks(int sortBy, ArrayList<ConfusionMark> confusionMarks){
+        switch (sortBy){
+            case SORT_BY_IS_RESOLVED:
+                confusionMarks.sort(new Comparator<ConfusionMark>() {
+                    @Override
+                    public int compare(ConfusionMark cm1, ConfusionMark cm2) {
+                        boolean cm1Resolved = cm1.getIsResolved();
+                        boolean cm2Resolved = cm2.getIsResolved();
+
+                        // If resolved status is the same, use start date as tie breaking
+                        if (cm1Resolved == cm2Resolved){
+                            return cm1.getStartDate().compareTo(cm2.getStartDate());
+                        }
+                        // Otherwise, have have the unresolved confusion mark show up first in the ordering
+                        else if (cm1Resolved && !cm2Resolved){
+                            return 1;
+                        }
+                        else{
+                            return -1;
+                        }
+                    }
+                });
+
+                break;
+            case SORT_BY_START_DATE:
+                confusionMarks.sort(new Comparator<ConfusionMark>() {
+                    @Override
+                    public int compare(ConfusionMark cm1, ConfusionMark cm2) {
+                        return cm1.getStartDate().compareTo(cm2.getStartDate());
+                    }
+                });
+                break;
+            default:
+                Log.e("Foo", "Invalid sorting criteria given to sorting questions list");
+                break;
+        }
+    }
+
 
     public interface TimestampClicked {
         void sendSlideNumber(int slideNumber);
