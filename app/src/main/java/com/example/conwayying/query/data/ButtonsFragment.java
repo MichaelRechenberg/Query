2ba312d66ec45f505f34fa4a209e9e2e3c0f4b82
emilyhowing <ehowing@gmail.com>
2018-12-06 13:06:14 -0600

    Changed the image button icons for WTF and Question and the icons on the bottom bar for timestamps and questions. Also, rounded corners on buttons.

--- app/src/main/java/com/example/conwayying/query/data/ButtonsFragment.java
+++ app/src/main/java/com/example/conwayying/query/data/ButtonsFragment.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
